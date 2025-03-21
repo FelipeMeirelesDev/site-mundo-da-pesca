990b3bac2d7ec3a05d2a525f429c0ccd6d67d601
Felipe Meireles <145486045+FelipeMeirelesDev@users.noreply.github.com>
2025-03-21 15:30:30 -0300

    Adicionando arquivos do spring

---
+++ src/main/java/com/mundodapesca/mundodapesca/MundodapescaApplication.java
@@ -0,0 +1,13 @@
+package com.mundodapesca.mundodapesca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundodapescaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundodapescaApplication.class, args);
+	}
+
+}
